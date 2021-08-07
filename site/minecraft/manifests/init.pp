d99df5220333dea60d8b740f18d3b853265e161f
Salustiano Muniz <stsmuniz@gmail.com>
2021-08-07 18:04:45 -0300

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -3,22 +3,22 @@ class minecraft {
     ensure => directory,
   }
   file {'/opt/minecraft/server.jar':
-    ensure => file
-    source => 'https://launcher.mojang.com/v1/objects/1b557e7b033b583cd9f66746b7a9ab1ec1673ced/server.jar'
+    ensure => file,
+    source => 'https://launcher.mojang.com/v1/objects/1b557e7b033b583cd9f66746b7a9ab1ec1673ced/server.jar',
   }
   package {'java':
     ensure => present,
   }
   file {'/opt/minecraft/eula.txt':
-    ensure => file
+    ensure => file,
     content => 'eula=true'
   }
   file {'/etc/systemd/system/minecraft.service':
-    ensure => file
+    ensure => file,
     source => 'puppet:///modules/minecraft/minecraft.service',
   }
   service {'minecraft':
-    ensure => 'running',
+    ensure => running,
     enable => true
   }
 }
