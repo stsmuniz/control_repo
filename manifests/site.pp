8a8a4eae7f1d49c5fff5a6d57d87fc4459dba687
Salustiano Muniz <stsmuniz@gmail.com>
2021-08-06 21:29:12 -0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,6 +1,7 @@
 node default {
   file {'/root/README':
-    ensure => file,
-    content => 'this is a readme'
+    ensure  => file,
+    content => 'this is a readme',
+    owner   => 'root'
   }
 }
