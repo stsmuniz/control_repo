fa38028cd97230a425e1038e54a3635884cfde27
Salustiano Muniz <stsmuniz@gmail.com>
2021-08-07 15:40:33 -0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,14 +1,15 @@
 node default {
 }
-
 node 'master.puppet.vm' {
   include role::master_server
+  file { '/root/README':
+    ensure => file,
+    content => $fqdn,
+  }
 }
-
 node /^web/ {
   include role::app_server
 }
-
 node /^db/ {
   include role::db_server
 }
